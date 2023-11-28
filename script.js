03acf1253495e10cbb2e1b629ee58375a0736ce5
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-28 10:18:52 -0800

    Started working on code

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// Questions to ask 
+
+// 1. Which programming language is primarily used in conjunction with SwiftUI for iOS app development?
+// Answer: SwiftUI is primarily used with the Swift programming language for iOS app development.
+
+
+// 2. Which SwiftUI view is used to create a button in an iOS app?
+// Answer: The Button view is used to create a button in SwiftUI.
+
+// 3. How is a list created in SwiftUI?
+// Answer: A list in SwiftUI is created using the List view, which displays a collection of identifiable data items.
+
+//4. How do you display text in a SwiftUI view?
+//A) Using the Text view
+
+
+//5. How can you change the color of text in a SwiftUI Text view?
+//With the foregroundColor modifier
+
+
+//6. What SwiftUI modifier can be used to make text bold in a Text view?
+//Bold()
+
+
+//7. Which layout container in SwiftUI arranges views in a vertical stack?
+//VStack
+
+
+//8. Which layout container in SwiftUI arranges views in a horizontal stack?
+//HStack
+
+//9. What syntax is primarily used to create a SwiftUI view hierarchy?
+//Swift code with a declarative approach
+
+//10. What year was SwiftUI first relazsed to the public?
+//2019
