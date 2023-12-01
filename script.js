4511d72e7a36b4adca4b56a251a58504e44ab3b5
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-01 10:23:49 -0800

    Made questions update and add color feedback when user selects and answer

--- script.js
+++ script.js
@@ -1,106 +1,107 @@
 const questions  = {
     0:{
-        question: "Which programming language is primarily used in conjunction with SwiftUI for iOS app development?",
-        answers:{
-            a:{option: "Java", correct: false},
-            b:{option: "JavaScript", correct: false},
-            c: {option: "SwiftUI ", correct: true},
-            d: {option: "C++", correct: false}
-        } 
+        question: "Which programming language is primarily used in conjunction with Swift for iOS app development?",
+        answers: [
+            {option: "Java", correct: false},
+            {option: "JavaScript", correct: false},
+            {option: "SwiftUI ", correct: true},
+            {option: "C++", correct: false}
+        ]
+        
     },
 
     1: {
         question: "Which SwiftUI view is used to create a button in an iOS app?", 
-        answers:{
-            a:{option: "Button", correct: true},
-            b:{option: "Text", correct: false},
-            c: {option: "Bold", correct: false},
-            d: {option: "VStack", correct: false}
-        }
+        answers: [
+            {option: "Button", correct: true},
+            {option: "Text", correct: false},
+            {option: "Bold", correct: false},
+            {option: "VStack", correct: false}
+        ]
     },
 
     2: {
         question:  "How is a list created in SwiftUI?",
-        answers: {
-            a:{option: "Button", correct: false},
-            b:{option: "List", correct: true},
-            c: {option: "Color", correct: false},
-            d: {option: "HStack", correct: false}
-        }
+        answers: [
+            {option: "Button", correct: false},
+            {option: "List", correct: true},
+            {option: "Color", correct: false},
+            {option: "HStack", correct: false}
+        ]
     },
 
 
     3: {
         question: "How do you display text in a SwiftUI view?", 
-        answers: {
-              a:{option: "TextView", correct: false},
-              b:{option: "Bold", correct: false},
-              c: {option: "Text", correct: true},
-              d: {option: "VStack", correct: false}
-        }
+        answers: [
+              {option: "TextView", correct: false},
+              {option: "Bold", correct: false},
+              {option: "Text", correct: true},
+              {option: "VStack", correct: false}
+        ]
 
     },
 
     4: {
         question: "How can you change the color of text in a SwiftUI Text view?",
-        answers: {
-                a:{option: "Color", correct: false},
-                b:{option: "Foreground Color", correct: true},
-                c: {option: "ColorView", correct: false},
-                d: {option: "Hex", correct: false}
-        }
+        answers: [
+                {option: "Color", correct: false},
+                {option: "Foreground Color", correct: true},
+                {option: "ColorView", correct: false},
+                {option: "Hex", correct: false}
+        ]
     }, 
 
     5: {
         question: "What SwiftUI modifier can be used to make text bold in a Text view?",
-        answers: {
-             a:{option: "Fontsize", correct: false},
-             b:{option: "Fontweight", correct: false},
-             c: {option: "Bold", correct: true},
-             d: {option: "italic", correct: false}
-           }
+        answers: [
+             {option: "Fontsize", correct: false},
+             {option: "Fontweight", correct: false},
+             {option: "Bold", correct: true},
+             {option: "italic", correct: false}
+        ]
     }, 
 
     6: {
         question: "Which layout container in SwiftUI arranges views in a vertical stack?",
-        answers: {
-            a:{option: "Vstack", correct: true},
-            b:{option: "Hstack", correct: false},
-            c: {option: "LazyVGrid", correct: false},
-            d: {option: "Foreach", correct: false}
-        }
+        answers: [
+            {option: "Vstack", correct: true},
+            {option: "Hstack", correct: false},
+            {option: "LazyVGrid", correct: false},
+            {option: "Foreach", correct: false}
+        ]
     }, 
 
     7: {
 
         question: "Which layout container in SwiftUI arranges views in a horizontal stack?",
-        answers: {
-            a:{option: "Vstack", correct: false},
-            b:{option: "Hstack", correct: true},
-            c: {option: "Lstack", correct: false},
-            d: {option: "Print", correct: false}
-        }
+        answers: [
+            {option: "Vstack", correct: false},
+            {option: "Hstack", correct: true},
+            {option: "Lstack", correct: false},
+            {option: "Print", correct: false}
+        ]
     }, 
 
     8: {
         question: "What year was SwiftUI first relazsed to the public?", 
-        answers: {
-             a:{option: "2018", correct: false},
-             b:{option: "2017", correct: false},
-             c: {option: "2019", correct: true},
-             d: {option: "2020", correct: false}
-        }
+        answers: [
+             {option: "2018", correct: false},
+             {option: "2017", correct: false},
+             {option: "2019", correct: true},
+             {option: "2020", correct: false}
+        ]
     }, 
 
     9: {
         question: "What IDE do you use to code with?", 
-        answers: {
-                a:{option: "Viusal Studio Code", correct: false},
-                b:{option: "Repliet", correct: false},
-                c: {option: "TextEditor", correct: false},
-                d: {option: "Xcode", correct: true}
-       }
-    }, 
+        answers: [
+                {option: "Viusal Studio Code", correct: false},
+                {option: "Repliet", correct: false},
+                {option: "TextEditor", correct: false},
+                {option: "Xcode", correct: true}
+        ]
+    }
 
 
 }
@@ -123,38 +124,85 @@ const nextQuestion = document.getElementById("NQ");
 
 
 let currentQuestion = 0;
+let timer;
 
 console.log(questions.answers)
 
+function startTimer() {
+    let timeLeft = 60
+    time = setInterval(function() {
+        if (timeLeft <= 0) {
+            clearInterval(timer)
+            console.log("Time is up for this question")
+        } else {
+            console.log("Time remaing for this question is", timeLeft)
+            timeLeft--;
+        }
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+}
+
+
 function LoadQuestion() {
   const question = questions[currentQuestion];
-  const options = question[currentQuestion];
-  console.log(options)
   const buttons = document.querySelectorAll(".center");
+  console.log(buttons)
   console.log("DEBUG:"+ question.question)
   questionTitle.innerText = question.question;
-  questionNumber.innerText = `Question ${currentQuestion + 1}`;
-  answerbutton1.innerText = options.currentQuestion.a
+  questionNumber.innerText = `Question ${currentQuestion + 1} out of 10`;
+  const answers = question.answers;
+  console.log(answers.answers)
+  console.log(answers)
+  answerbutton1.innerText = question.answers.a
+//   answerbutton1.innerText = options.currentQuestion.a
 //   console.log(questionNumber)
   console.log(questionTitle)
+  buttons.forEach(button => {
+    button.innerHTML = '';
+    button.classList.remove("answeriscorrect", "answerisincorrect")
+    button.disabled = false;
+  })
 
-  for(let i = 0; i < answerbutton1.length; i++) {
-    answerbutton1[i].textContent = question.answers[i].option;
-    answerbutton1[i].onclick = function() {
-
+  for(let i = 0; i < answers.length; i++) {
+    buttons[i].innerText = question.answers[i].option;
+    buttons[i].onclick = function() {
+        buttons.innerText = question.answers
+        if(answers[i].correct) {
+              console.log("Correct answer for" + question.currentQuestion)
+              buttons[i].classList.add("answeriscorrect")
+              buttons.classList.remove()
+              stopTimer()
+              
+        } else {
+            console.log("Incorrect answer for" + question.currentQuestion)
+            buttons[i].classList.add("answerisincorrect")
+            buttons.classList.remove()
+            stopTimer()
+        }
+        
     };
   }
 
+  
 }
 
 nextQuestion.addEventListener("click", function() {
     currentQuestion++;
-    if(currentQuestion < questions.length) {
+    console.log("Next question has been clicked")
+    if(currentQuestion < 10) {
         LoadQuestion();
-        questionTitle.innerText = question.question;
+        startTimer();
+        // questionTitle.innerText = 
+        
     } else {
-
+       stopTimer()
     }
 });
 
+
+
 LoadQuestion();
+startTimer();
